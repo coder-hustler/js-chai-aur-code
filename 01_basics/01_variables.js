bb5921215234ba7c8c1af8ee1e0edf5fba78a04d
Prem Aggarwal <155379276+coder-hustler@users.noreply.github.com>
2024-07-07 09:53:43 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accountEmail = 'prem@google.com';
+var accountPassword = '1111';
+let accountState;
+
+console.log(accountState);
+/*
+Prefer not to use var because of 
+issues in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+console.log(typeof null);
+console.log(typeof undefined);
+
+// number
+// string
+// boolean
+// null
+// undefined
+// symbol, used for uniqueness
+// object
